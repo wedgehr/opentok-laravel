9839735acc9058482daa28a059b62c9eb0760f21
Patrick McCarren <patrick@mmsc.io>
2017-12-22 14:56:47 -0500

    WIP.feat: Multi Project Support

--- config/opentok.php
+++ config/opentok.php
@@ -1,6 +1,23 @@
-<?php 
+<?php
 
-return	[
-    'api_key' => 'API_KEY',
-    'api_secret' => 'API_SECRET',
+return [
+	// default project
+	'default' => 'default',
+
+	// a list of the projects we know about
+	'projects' => [
+		'default' => [
+			'api-key' => 'TOKBOX_DEFAULT_API_KEY',
+			'api-secret' => 'TOKBOX_DEFAULT_API_SECRET',
+		],
+
+		/**
+		 * Example additional project
+		 *
+		 * 'safari' => [
+		 *     'api-key' => 'TOKBOX_SAFARI_API_KEY',
+		 *     'api-secret' => 'TOKBOX_SAFARI_API_SECRET',
+		 * ],
+		 */
+	],
 ];
